d855557666eb957c2f0deede4fe825c4d3efd1f4
Alussius Aloy <aaloy@my.centennialcollege.ca>
2020-07-24 22:45:20 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <!-- Passing as multiple slots to child component -->
+          <h2 slot="title">{{ quote.title }}</h2>
+          <p slot="content">{{ quote.content }}</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue';
+
+export default {
+  data() {
+    return {
+      quote: { title: 'Test Quote', content: 'A Wonderful Quote' }
+    }
+  },
+  components: {
+    appQuote: Quote
+  }  
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="title">
+            <slot name="title"></slot>
+            <!-- Slots default state in case no subtitle data from the parent component -->
+            <span><slot name="subtitle">No Subtitle</slot></span>
+        </div>
+        <hr>
+        <div>
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['quote']
+}
+</script>
+
+<style scoped>
+div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+}
+span{
+    color: #ccc;
+}
+</style>
