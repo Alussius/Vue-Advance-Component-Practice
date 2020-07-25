a3d47175ba43d9c66323e91fff6e5e262e69aed3
Alussius Aloy <aaloy@my.centennialcollege.ca>
2020-07-24 23:26:52 -0400

    adding dynamic components

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,21 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
+        <button @click="selectedComp = 'appQuote'">Quote</button>
+        <button @click="selectedComp = 'appAuthor'">Author</button>
+        <button @click="selectedComp = 'appNew'">New</button>
+        <hr>
+        <p>{{ selectedComp }}</p>
         <app-quote>
+          <!-- Commented out to introduce Dynamic Components -->
           <!-- Passing as multiple slots to child component -->
-          <h2 slot="title">{{ quote.title }}</h2>
-          <p slot="content">{{ quote.content }}</p>
+          <!-- <h2 slot="title">{{ quote.title }}</h2>
+          <p slot="content">{{ quote.content }}</p> -->
+
+          <!-- Dynamic Component -->
+          <component :is="selectedComp">
+            <p>test</p>
+          </component>
         </app-quote>
       </div>
     </div>
@@ -14,15 +25,20 @@
 
 <script>
 import Quote from './components/Quote.vue';
+import Author from './components/Author.vue';
+import New from './components/New.vue';
 
 export default {
   data() {
     return {
-      quote: { title: 'Test Quote', content: 'A Wonderful Quote' }
+      quote: { title: 'Test Quote', content: 'A Wonderful Quote' },
+      selectedComp: 'appQuote'
     }
   },
   components: {
-    appQuote: Quote
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New
   }  
 }
 </script>
---
+++ src/components/Author.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h3>Author Component</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+</style>
